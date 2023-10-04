8f82ee7e088b091841be2077dfd2ebc9bf72dad1
vikkuccine <kamenkova09@gmail.com>
2023-10-04 22:27:47 +0300

    add dots toggle

--- app/js/main.js
+++ app/js/main.js
@@ -18,3 +18,18 @@ class Tabs {
 
 
 const tabs = new Tabs('.catalog-list');
+
+
+
+const dotsBtn = document.querySelectorAll('.dots__item')
+
+dotsBtn.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    const activeDot = event.target.parentElement.querySelector('.dots__item--active')
+    activeDot.classList.remove('dots__item--active')
+    event.target.classList.add('dots__item--active')
+  })
+
+})
+
+
