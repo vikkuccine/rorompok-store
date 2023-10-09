e45fb8fed042e435097bd522c770660c5e91f0f3
vikkuccine <kamenkova09@gmail.com>
2023-10-09 20:06:44 +0300

    add img changer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,3 @@
-
 class Tabs {
   constructor(selector) {
     this.selector = selector;
@@ -21,15 +20,48 @@ const tabs = new Tabs('.catalog-list');
 
 
 
-const dotsBtn = document.querySelectorAll('.dots__item')
+class DotsToggle {
+  constructor(dotsItem, img, imgContainer) {
+    this.dotsItem = document.querySelectorAll(dotsItem);
+    this.img = img;
+    this.imgContainer = imgContainer;
+    this.initEventListener();
+  }
 
-dotsBtn.forEach((btn) => {
-  btn.addEventListener('click', (event) => {
-    const activeDot = event.target.parentElement.querySelector('.dots__item--active')
+  initEventListener() {
+    this.dotsItem.forEach((btn) => {
+      btn.addEventListener('click', (event) => {
+        console.log(event.target.parentElement.parentElement);
+        const eventTarget = event.target
+        this.changeDot(eventTarget)
+        this.changeImg(eventTarget)
+      })
+
+    })
+  }
+
+  changeDot(eventTarget) {
+    const activeDot = eventTarget.parentElement.querySelector('.dots__item--active')
     activeDot.classList.remove('dots__item--active')
-    event.target.classList.add('dots__item--active')
-  })
+    eventTarget.classList.add('dots__item--active')
+  }
 
-})
+  changeImg(eventTarget) {
+    console.log('eventTarget.closest(this.imgContainer)', eventTarget.closest(this.imgContainer));
+    const currentImg = eventTarget.closest(this.imgContainer).querySelector(this.img);
+    const currentMicroPhoto = currentImg.getAttribute('src')
+    // const findImg = eventTarget.parentElement
+    
+    const newPhoto = eventTarget.dataset.img;
+    if (currentMicroPhoto !== newPhoto) {
+      currentImg.setAttribute('src', newPhoto)
+    }
+
+  }
+}
+
+const microDots = new DotsToggle('.micro-card .dots__item', '.micro-card__img', '.micro-card');
+
+const catalogDots = new DotsToggle('.catalog-list .dots__item', '.catalog-list__img', '.catalog-list__item');
 
 
